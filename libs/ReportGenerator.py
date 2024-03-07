fb2444367073fe484e17d35457f9f7895ac80c51
Pak Cyberbot <93365275+PakCyberbot@users.noreply.github.com>
2024-03-07 20:55:25 +0500

    implemented images in array templating too
    now only need to support image url with key = "Picture", right now array
    template support single image

--- libs/ReportGenerator.py
+++ libs/ReportGenerator.py
@@ -1,6 +1,27 @@
-import os, shutil, re
+import os, shutil, re, requests
 import zipfile, tempfile
+def download_image(url):
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
 
+        # Create a temporary directory
+        temp_dir = tempfile.mkdtemp()
+
+        # Create a temporary file to save the image
+        temp_file_path = os.path.join(temp_dir, 'image.jpg')
+
+        # Write the image content to the temporary file
+        with open(temp_file_path, 'wb') as f:
+            f.write(response.content)
+
+        # Return the path of the downloaded image and the temporary directory
+        return temp_file_path, temp_dir
+    except requests.exceptions.RequestException as e:
+        print("Error downloading image:", e)
+        return None, None
+    
 def substitute_values(stringdata, dictdata):
             # regex pattern to find placeholder and replace with the value
             for placeholder, value in dictdata.items():
@@ -43,6 +64,42 @@ def substitute_values(stringdata, dictdata):
                     stringdata = re.sub(rf'&lt;{placeholder}&gt;', value, stringdata)
             return stringdata
 
+def adding_an_image(unzippedocx_dir, img_url, img_no, content):
+    # Use re.sub to replace the ID value for adding new image
+    img_dir = os.path.join(unzippedocx_dir,'word','media')
+    file_names = os.listdir(img_dir)
+
+    img_pos = len(file_names) + 1
+    img_name = f"image{img_pos}.png"
+    image_path, temp_dir = download_image(img_url)
+    shutil.copy(image_path,os.path.join(img_dir, img_name))
+    shutil.rmtree(temp_dir)
+
+    img_id_pattern = r'id="(\d+)" name="Picture (\d+)"'
+    rid_pattern = r'<a:blip r:embed="rId(\d+)"/>'
+
+    img_org_id = int(re.search(img_id_pattern, content).group(1))
+    rid_num = int(re.search(rid_pattern, content).group(1))
+
+    extracted_data = re.sub(img_id_pattern, f'id="{img_pos}" name="Picture {img_pos}"', content)
+    extracted_data = re.sub(rid_pattern, f'<a:blip r:embed="rId{rid_num + (img_pos - img_org_id )}"/>', extracted_data)
+    
+    doc_rels_path = os.path.join(unzippedocx_dir,'word','_rels',"document.xml.rels")
+    with open(doc_rels_path, 'r') as content_file:
+        rels_content = content_file.read()
+    
+    rels_pretext_pattern = r'<\?xml version="1.0" encoding="UTF-8" standalone="yes"\?>.*?<Relationships.*?rId(\d+).*?Target="fontTable\.xml"/>'
+
+    max_rid = int(re.search(rels_pretext_pattern, rels_content,  re.DOTALL).group(1))
+    pretext = f'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId{max_rid+1}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/fontTable" Target="fontTable.xml"/>'
+    new_text = pretext + f'<Relationship Id="rId{rid_num + (img_pos - img_org_id)}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/image{img_pos}.png"/>'
+    rels_content = re.sub(rels_pretext_pattern, "", rels_content,  flags=re.DOTALL)
+    rels_content = new_text + rels_content
+
+    with open(doc_rels_path, 'w', encoding="utf-8") as modified_file:
+        modified_file.write(rels_content)
+    
+    return extracted_data
 def reportme(tmpl_path,out_path,data_dict, img_dict=None, array=None):
     """
     fill_template(tmpl_path, out_path, data_dict, img_list):
@@ -124,10 +181,12 @@ def reportme(tmpl_path,out_path,data_dict, img_dict=None, array=None):
                 # Extract the first match (assuming there's only one) and strip leading/trailing whitespace
                 extracted_data = matches[0].strip(r"%array%").strip(r"%/array%")
                 new_data = ""
-                for dict_data in array:
-                    print("--------------------------------")
-                    new_data += substitute_values(extracted_data, dict_data)
-                    print(new_data)
+                for i, dict_data in enumerate(array):
+                    img_url = dict_data.pop('Picture', None)
+                    if img_url is not None:
+                        new_data += substitute_values(adding_an_image(temp_dir, img_url, i, extracted_data), dict_data)
+                    else:
+                        new_data += substitute_values(extracted_data, dict_data)
                 # Replace the original extracted data with the processed data
                 content = content.replace(f'%array%{extracted_data}%/array%', new_data)
 
@@ -170,4 +229,5 @@ if __name__ == "__main__":
     
     # reportme("openoffice.odt","test.odt",{"Heading": "Fing Video\nnew","Description":"checking\nthis\nvideo"})
 
-    reportme("libs/test_array.docx","array_out.docx",{"heading": "Fing Video\nnew","description":"checking\nthis\nvideo"},array=[{"vid":"tester1","num":"124"},{"vid":"nice","num":"1"},{"vid":"cat","num":"3"},{"vid":"crip","num":"1"}])
+    reportme("libs/test_array.docx","array_out.docx",{"heading": "Fing Video\nnew","description":"checking\nthis\nvideo"},array=[{"vid":"tester1","num":"124", "Picture":"https://yt3.googleusercontent.com/6FqcWoHZvrZixaGi1S3Re3Z90SCS3iq2_36hQSnSHQPtQVVkywH8WKka53MiBYBSP6DmqM-g9w=s176-c-k-c0x00ffffff-no-rj"},{"vid":"nice","num":"1","Picture":"https://yt3.googleusercontent.com/mHrqjdngHeZfR08mkdIcSiL7vWFLAhrrPrRutOQJYQ1RWRKPEVDsxKVfL_nIxZg5njr-lQAqQg=s176-c-k-c0x00ffffff-no-rj"},{"vid":"cat","num":"3", "Picture":"https://yt3.ggpht.com/ZeokXdjeXW_6CpcChqvVBEBcHoJ9TAaLTnQj8yT942LLV8afhmUv6zLtqzbNS1uPnernj3SPshA=s68-c-k-c0x00ffffff-no-rj"},{"vid":"crip","num":"1"}])
+    # reportme("libs/test_array.docx","array_out.docx",{"heading": "Fing Video\nnew","description":"checking\nthis\nvideo"},array=[{"vid":"tester1","num":"124"},{"vid":"nice","num":"1"},{"vid":"cat","num":"3"},{"vid":"crip","num":"1"}])
